423a820b0a825d8d9896a4757c71ac7bdf4e6943
fanlixingit <78702670+fanlixingit@users.noreply.github.com>
2021-02-07 18:33:59 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,19 +11,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+		<h1>Hello CodeSandbox</h1>
+		<h2>Start editing to see some magic happen!</h2>
+  		</header>
       </div>
     );
   }
