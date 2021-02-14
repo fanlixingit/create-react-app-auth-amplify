aa6476c7e07a7b5aaf46d2a3449182fc49227ae4
fanlixingit <78702670+fanlixingit@users.noreply.github.com>
2021-02-14 17:20:06 -0500

    Remove local video

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import { withAuthenticator } from 'aws-amplify-react'
-// import Amplify, { Auth } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react'
+import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
@@ -11,12 +11,19 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          Lixin First Glassboro website.
+        <body>
+        <video width="320" height="240" controls>
+      <source src="https://www.youtube.com/watch?v=nS7HJuXOv-4&ab_channel=offcampusrental" type='video/mp4'>
+    </video>
+    <p>Glassboro's finest! Walking Distance to Rowan University. Large living room and dining room and kitchen are ready for your party gathering. Need more space the lower level has a family room with half bath and large laundry room. A screened in back patio that extends to the entire back of the home gives you ample outdoor entertaining in warmer weather. 
+      The upper level has 3 large bedrooms with spacious closets and full bathroom. The maintenance-free exterior home sits on a beautifully wooded property with gorgeous trees 
+      and shrubs. This home will not be on the market long. Don't delay make an appointment today! Make sure to watch the video tour!</p>
+
+    </body>
         </header>
       </div>
     );
   }
 }
 
-export default withAuthenticator(App, true);
+export default withAuthenticator(App, false);
