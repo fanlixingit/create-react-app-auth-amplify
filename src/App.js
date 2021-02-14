da6804690f7378075cbb9d2854080f34aeeb6b74
fanlixingit <78702670+fanlixingit@users.noreply.github.com>
2021-02-14 18:37:18 -0500

    Fix Google map

--- src/App.js
+++ src/App.js
@@ -14,15 +14,25 @@ class App extends Component {
         <body>
         <iframe width="840" height="630"
           src="https://www.youtube.com/embed/nS7HJuXOv-4">
-</iframe>
+        </iframe>
 
-<div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=940%20N%20main%20street,%20glassboro.%20nj&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-to.org">123movies</a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style><a href="https://www.embedgooglemap.net">google maps in html</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div></div>
+      <div class="mapouter">
+        <div class="gmap_canvas">
+          <iframe width="600" height="500" id="gmap_canvas" 
+              src="https://maps.google.com/maps?q=940%20N%20main%20street,%20glassboro.%20nj&t=&z=13&ie=UTF8&iwloc=&output=embed" 
+              frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+          </iframe>
+          
+          <br>
+         
+        </div>
+      </div>
 
     <p>Glassboro's finest! Walking Distance to Rowan University. Large living room and dining room and kitchen are ready for your party gathering. Need more space the lower level has a family room with half bath and large laundry room. A screened in back patio that extends to the entire back of the home gives you ample outdoor entertaining in warmer weather. 
       The upper level has 3 large bedrooms with spacious closets and full bathroom. The maintenance-free exterior home sits on a beautifully wooded property with gorgeous trees 
       and shrubs. This home will not be on the market long. Don't delay make an appointment today! Make sure to watch the video tour!</p>
 
-    </body>
+        </body>
         </header>
       </div>
     );
