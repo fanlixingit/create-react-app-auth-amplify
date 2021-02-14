413b11edff600153ffdb58180ed0d1fd5733ecd2
fanlixingit <78702670+fanlixingit@users.noreply.github.com>
2021-02-14 17:26:23 -0500

    Fix video link

--- src/App.js
+++ src/App.js
@@ -12,10 +12,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
         <body>
-        <video width="320" height="240" controls>
-          
-      <source src="https://www.youtube.com/watch?v=nS7HJuXOv-4&ab_channel=offcampusrental" type='video/mp4'>
-    </video>
+        <iframe width="420" height="315"
+src="https://www.youtube.com/watch?v=nS7HJuXOv-4&ab_channel=offcampusrental">
+</iframe>
+
+
     <p>Glassboro's finest! Walking Distance to Rowan University. Large living room and dining room and kitchen are ready for your party gathering. Need more space the lower level has a family room with half bath and large laundry room. A screened in back patio that extends to the entire back of the home gives you ample outdoor entertaining in warmer weather. 
       The upper level has 3 large bedrooms with spacious closets and full bathroom. The maintenance-free exterior home sits on a beautifully wooded property with gorgeous trees 
       and shrubs. This home will not be on the market long. Don't delay make an appointment today! Make sure to watch the video tour!</p>
